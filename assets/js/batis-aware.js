3d18c1f81bf2fa0034367730eedc2b45802c92cd
chojisan <choji.takada@gmail.com>
2018-11-04 22:02:03 +0800

    modified fonts and add back to top

--- assets/js/batis-aware.js
+++ assets/js/batis-aware.js
@@ -47,4 +47,37 @@
     $('.navbar').removeClass('d-none');
   })
 
+  //go to top
+  var goToTop = function() {
+
+    $('.js-gotop').on('click', function(event){
+      
+      event.preventDefault();
+
+      $('html, body').animate({
+        scrollTop: $('html').offset().top
+      }, 500, 'easeInOutExpo');
+      
+      return false;
+    });
+
+    $(window).scroll(function(){
+
+      var $win = $(window);
+      if ($win.scrollTop() > 200) {
+        $('.js-top').addClass('active');
+      } else {
+        $('.js-top').removeClass('active');
+      }
+
+    });
+  
+  };
+
+  $(function(){
+
+    goToTop();
+
+  });
+
 })(jQuery); // End of use strict
